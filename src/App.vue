7f6d054eb8f7debf4a5f70118d9edcd1e245a0ba
pengliang <953627561@qq.com>
2025-12-01 16:23:36 +0800

    添加路由密码

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { computed } from 'vue'
+import PasswordGuard from './components/PasswordGuard.vue'
+
 // 移除了 lib-flexible 相关代码，改用纯 CSS 响应式方案
+
+// 检查认证状态
+const isAuthenticated = computed(() => {
+  const authStatus = localStorage.getItem('bp_english_auth')
+  return authStatus === 'authenticated'
+})
 </script>
 
 <template>
-	<router-view />
+	<PasswordGuard v-if="!isAuthenticated" />
+	<router-view v-else />
 </template>
 
 <style scoped>
